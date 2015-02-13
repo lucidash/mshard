fceab04ba8559aaf4bd7bf4a62eb368211a5362a
Minwoo Lee <ermaker@gmail.com>
2015-02-13 11:06:13 +0900

    Add pushbullet spec

--- spec/lib/mshard/mshard_spec.rb
+++ spec/lib/mshard/mshard_spec.rb
@@ -5,4 +5,15 @@ describe MShard::MShard do
     id = subject.set(contents: 'a')
     expect(subject.get(id)).to eq('a')
   end
+
+  it 'works' do
+    subject.set(
+      pushbullet: true,
+      channel_tag: ENV['PUSHBULLET_CHANNEL'],
+      type: 'link',
+      title: 'title',
+      body: 'body',
+      contents: 'a'
+    )
+  end
 end
