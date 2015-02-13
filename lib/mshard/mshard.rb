298148ca11489c8d9f674ce9d50bbbbacd2c0e08
Minwoo Lee <ermaker@gmail.com>
2015-02-13 10:13:41 +0900

    Apply protocol v2

--- lib/mshard/mshard.rb
+++ lib/mshard/mshard.rb
@@ -6,11 +6,11 @@ module MShard
     base_uri ENV['MSHARD_URI']
 
     def get(id)
-      self.class.get("/shards/#{id}")['contents']
+      self.class.get("/v2/shards/#{id}").body
     end
 
     def set(params)
-      self.class.post('/shards', body: params)['id']
+      self.class.post('/v2/shards', body: params)['id']
     end
   end
 end
