cb9a6d75b246465e90cb0a8bba4a6ab5c55c53c6
Minwoo Lee <ermaker@gmail.com>
2015-07-30 21:43:59 +0900

    Remove unused statements

--- spec/lib/mshard/mshard_spec.rb
+++ spec/lib/mshard/mshard_spec.rb
@@ -1,22 +1,6 @@
 require 'mshard/mshard'
 
 describe MShard::MShard do
-  xit 'works' do
-    id = subject.set(contents: 'a')
-    expect(subject.get(id)).to eq('a')
-  end
-
-  xit 'works' do
-    subject.set(
-      pushbullet: true,
-      channel_tag: ENV['PUSHBULLET_CHANNEL'],
-      type: 'link',
-      title: 'title',
-      body: 'body',
-      contents: 'a'
-    )
-  end
-
   it '#try works' do
     expect(subject.try { :result }).to be(:result)
     expect(subject.try(delay: 0.01) { fail }).to be(nil)
