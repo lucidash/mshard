dea9f41336a5447d7cf469017aa48e3c30628dbf
Minwoo Lee <ermaker@gmail.com>
2015-07-30 21:18:39 +0900

    Add an util shellfile

---
+++ cov.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xe
+
+CWD=$(pwd $(dirname $0))
+COV=${CWD}/coverage
+
+COVERAGE=true rake spec
+
+docker run -it --rm \
+  -v ${COV}:/usr/share/nginx/html:ro \
+  -p 8080:80 \
+  nginx
