184e949382adf5a3856c9372ee78957ec0e9b06d
Minwoo Lee <ermaker@gmail.com>
2015-07-30 20:57:36 +0900

    Add Codecov

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://rubygems.org'
 
 # Specify your gem's dependencies in mshard.gemspec
 gemspec
+
+gem 'codecov', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,20 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+if ENV['TRAVIS'] || ENV['COVERAGE']
+  require 'simplecov'
+
+  if ENV['TRAVIS']
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
