a09cf4fcf8137f7079fc99271519cbee6b32bb10
Minwoo Lee <ermaker@gmail.com>
2015-07-30 21:18:53 +0900

    Cover #error_to_html

--- spec/lib/mshard/mshard_spec.rb
+++ spec/lib/mshard/mshard_spec.rb
@@ -16,4 +16,15 @@ describe MShard::MShard do
       contents: 'a'
     )
   end
+
+  it '#error_to_html works' do
+    e = begin
+          fail
+        rescue => e
+          e
+        end
+    html = (subject.error_to_html(e))
+    expect(html).to include('<title>Error</title>')
+    expect(html).to include('<pre>RuntimeError</pre>')
+  end
 end
