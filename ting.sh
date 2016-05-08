ebacd5659c96942f4cf9f62c28c20810624a020b
Minwoo Lee <ermaker@gmail.com>
2016-05-08 13:43:15 +0900

    Add argument support

--- ting.sh
+++ ting.sh
@@ -1,4 +1,7 @@
-TITLE=$1
-shift
+TITLE=${1:-Ting!}
+if [ $# -ge 1 ]
+then
+  shift
+fi
 BODY="$*"
 curl -XPOST $MSHARD_URI/v2/shards --data pushbullet=true --data type=note --data title="$TITLE" --data body="$BODY"
